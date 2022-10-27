6b7082c3ec470fa054577cb1803a2d6ebfc5a439
choijongan <mmm7134@naver.com>
2022-10-27 21:19:17 +0900

    왼쪽 UI디자인

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import ContactForm from './component/ContactForm';
 
 //1. 왼쪽에는 연락처 등록하는 폼이, 오른쪽에는 연락처 리스트와 search창이 있다.
 //2. 리스트에 유저 이름과 전화번호를 추가할 수 있다.
@@ -8,7 +11,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
- 
+      <h1 className='title'>연락처</h1>
+      <Container>
+        <Row>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
