8e853faef2bb427b0cf805d74fc83b7ba0c7e4d8
choijongan <mmm7134@naver.com>
2022-11-03 23:44:41 +0900

    reducer를 이용한 연락처추가

--- phonebook/src/component/ContactForm.js
+++ phonebook/src/component/ContactForm.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useDispatch } from 'react-redux';
 
 const ContactForm = () => { 
   const [name,setName]=useState('')
   const [phoneNumber,setPhoneNumber]=useState(0);
+  const dispatch = useDispatch();
+
+  const addContact=(event)=> {
+    event.preventDefault();
+    dispatch({type:"ADD_CONTACT" , payload:{name, phoneNumber}})
+  }
  
   return (
-    //onchange=> 타입할 때마다 타입한 값을 읽어와서 저장
+    //onChange=> 타입할 때마다 타입한 값을 읽어와서 저장
     <div>
-    <Form>
+    <Form onSubmit={addContact}>
       <Form.Group className="mb-3" controlId="formName">
         <Form.Label>이름</Form.Label>
         <Form.Control type="text" placeholder="이름을 입력해주세요" 
